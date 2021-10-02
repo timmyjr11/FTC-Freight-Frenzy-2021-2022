62ad933491d1bc9dca1072dca91011bdf1c4b424
timmyjr11 <timmyytjr@gmail.com>
2021-10-01 20:23:05 -0700

    code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/idk.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+@Autonomous
+public class idk extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addLine("LOL");
+        telemetry.update();
+        waitForStart();
+        while (opModeIsActive()) {
+            telemetry.addLine("AH");
+            telemetry.update();
+        }
+    }
+}
