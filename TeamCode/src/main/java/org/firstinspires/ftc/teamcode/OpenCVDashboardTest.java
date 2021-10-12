8ffa55eb9a6b83fb730eede943d97db6b1cdb4e6
timmyjr11 <timmyytjr@gmail.com>
2021-10-11 19:26:11 -0700

    Added OpenCV/Dashboard Testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVDashboardTest.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera2;
+
+@Autonomous
+public class OpenCVDashboardTest extends LinearOpMode {
+
+    OpenCvInternalCamera2 cam;
+
+     private final FtcDashboard dash = FtcDashboard.getInstance();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        telemetry = new MultipleTelemetry(telemetry, dash.getTelemetry());
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        cam = OpenCvCameraFactory.getInstance().createInternalCamera2(OpenCvInternalCamera2.CameraDirection.BACK, cameraMonitorViewId);
+
+        cam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                cam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                cam.closeCameraDevice();
+            }
+        });
+
+        FtcDashboard.getInstance().startCameraStream(cam, 30);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            telemetry.update();
+        }
+    }
+}
