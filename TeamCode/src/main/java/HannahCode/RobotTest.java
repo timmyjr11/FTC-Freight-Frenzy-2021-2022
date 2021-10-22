3c9166fb3bbce27c31a0b0a424e9bb4289626a80
timmyjr11 <timmyytjr@gmail.com>
2021-10-22 12:17:35 -0700

    HannahBanana

--- TeamCode/src/main/java/HannahCode/RobotTest.java
+++ TeamCode/src/main/java/HannahCode/RobotTest.java
@@ -1,8 +1,11 @@
 package HannahCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@TeleOp
 public class RobotTest extends LinearOpMode {
     DcMotor frontLeft;
     DcMotor frontRight;
@@ -16,6 +19,9 @@ public class RobotTest extends LinearOpMode {
         backLeft = hardwareMap.get(DcMotor.class,"backLeft");
         backRight = hardwareMap.get(DcMotor.class,"backRight");
 
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
         waitForStart();
 
         while (opModeIsActive()) {
@@ -23,6 +29,11 @@ public class RobotTest extends LinearOpMode {
             frontRight.setPower(-gamepad1.left_stick_y);
             backLeft.setPower(-gamepad1.left_stick_y);
             backRight.setPower(-gamepad1.left_stick_y);
+
+            frontLeft.setPower(gamepad1.right_stick_x);
+            frontRight.setPower(-gamepad1.right_stick_x);
+            backLeft.setPower(gamepad1.right_stick_x);
+            backRight.setPower(-gamepad1.right_stick_x);
         }
 
     }
