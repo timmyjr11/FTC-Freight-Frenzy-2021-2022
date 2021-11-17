3f1c5106478c68c59a74fd4acebfa685a8ae6ec5
timmyjr11 <timmyytjr@gmail.com>
2021-11-16 18:18:52 -0700

    Installed dat git

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/continuousServoTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous
+public class continuousServoTest extends LinearOpMode {
+
+    CRServo continuous;
+    CRServo continuous2;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        continuous = hardwareMap.get(CRServo.class, "continuous");
+        continuous2 = hardwareMap.get(CRServo.class, "continuous2");
+
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            continuous.setPower(1);
+            continuous2.setPower(1);
+        }
+    }
+}
