25f1c1fa33afb0f0481b8f4ec99555f4d9725aec
timmyjr11 <timmyytjr@gmail.com>
2021-11-30 18:40:25 -0700

    WebCam OpenCV

---
+++ TeamCode/src/main/java/testCodes/usbCamTest.java
@@ -0,0 +1,53 @@
+package testCodes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@Autonomous
+public class usbCamTest extends LinearOpMode {
+
+    OpenCvWebcam cam;
+
+    private final FtcDashboard dash = FtcDashboard.getInstance();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        telemetry = new MultipleTelemetry(telemetry, dash.getTelemetry());
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        cam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam"), cameraMonitorViewId);
+
+        cam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                cam.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                cam.closeCameraDevice();
+
+            }
+        });
+
+        FtcDashboard.getInstance().startCameraStream(cam, 30);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()){
+            telemetry.update();
+        }
+
+    }
+}
