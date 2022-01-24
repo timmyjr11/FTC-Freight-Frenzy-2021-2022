6443436d2e3c2920deb8b2bca6d78baf83123f46
timmyjr11 <timmyytjr@gmail.com>
2022-01-23 20:01:26 -0700

    Configuration options for red side completed and commented the auto for clarity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/autoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/autoRed.java
@@ -24,31 +24,39 @@ import org.openftc.easyopencv.OpenCvWebcam;
 @Config
 @Autonomous
 public class autoRed extends LinearOpMode {
-
+//Creates the dashboard that is used for debugging
     private final FtcDashboard dashboard = FtcDashboard.getInstance();
 
+//Creates SampleMecanumDrive which allows the use of roadRunner
     SampleMecanumDrive d;
 
-    static int position;
+//Creates the integer 'position' that is used for the ducks in openCV
+    int position;
 
+//Creates the webcam
     OpenCvWebcam cam;
 
+//The starting Pose for roadRunner
     Pose2d start;
 
+//Creates integers that will be used for the auto configuration selector.
     int startingPosition;
     int parkingPosition;
     int warehousePosition;
 
+//Creates the left rectangle for openCV
     public static int rectLeftx = 65;
     public static int rectLefty = 385;
     public static int rectLeftWidth = 55;
     public static int rectLeftHeight = 55;
 
+//Creates the right rectangle for openCv
     public static int rectRightx = 570;
     public static int rectRighty = 390;
     public static int rectRightWidth = 55;
     public static int rectRightHeight = 55;
 
+//Creates the center rectangle for openCV
     public static int rectCenterx = 330;
     public static int rectCentery = 385;
     public static int rectCenterWidth = 55;
@@ -56,13 +64,17 @@ public class autoRed extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+//Declare the hardware map using 'SampleMecanumDrive'
         d = new SampleMecanumDrive(hardwareMap);
 
+//Allows the dashboard to record telemetry
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
 
+//Hardware maps the webcam and create a way to view what the camera sees
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         cam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
+//Opens the camera and sets the openCV code to the webcam
         cam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
@@ -70,6 +82,7 @@ public class autoRed extends LinearOpMode {
                 cam.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
             }
 
+//Runs if the camera fails to open
             @Override
             public void onError(int errorCode) {
                 cam.closeCameraDevice();
@@ -78,9 +91,21 @@ public class autoRed extends LinearOpMode {
             }
         });
 
+//Allows the dashboard to see what the camera sees
         FtcDashboard.getInstance().startCameraStream(cam, 30);
 
 
+//Lets roadRunner understand where the robot is on the field
+        d.setPoseEstimate(start);
+
+
+//Sets up servos for the proper positions
+        d.leftLinkage.setPosition(0);
+        d.rightLinkage.setPosition(0);
+        d.rightBox.setPosition(0);
+        d.leftBox.setPosition(0);
+
+//Creating the auto configuration
         startingPosition();
         sleep(500);
         parkingPosition();
@@ -89,15 +114,7 @@ public class autoRed extends LinearOpMode {
             wareHousePosition();
         }
 
-        d.setPoseEstimate(start);
-
-
-
-        d.leftLinkage.setPosition(0);
-        d.rightLinkage.setPosition(0);
-        d.rightBox.setPosition(0);
-        d.leftBox.setPosition(0);
-
+//After configuration is complete, the auto configuration is then read back to drivers to ensure the correct configuration
         telemetry.addLine("Current configuration:");
         if (startingPosition == -1) {
             telemetry.addLine("Left side");
@@ -130,6 +147,8 @@ public class autoRed extends LinearOpMode {
         telemetry.update();
 
         if (isStopRequested()) return;
+
+//TODO: Come back to comment what the Trajectories do
 
         Trajectory leftSideToCarousel = d.trajectoryBuilder(start)
                 .lineToConstantHeading(new Vector2d(-58, -58))
@@ -190,14 +209,15 @@ public class autoRed extends LinearOpMode {
 
 
         waitForStart();
-
+//When the robot has started, the camera stops streaming
         cam.stopStreaming();
 
 
 
     }
 
-     static class duckDetector extends OpenCvPipeline {
+//The openCV code that detects ducks
+     class duckDetector extends OpenCvPipeline {
         //Creates the YCbCr color space as a mat
         Mat HSV = new Mat();
 
@@ -214,37 +234,46 @@ public class autoRed extends LinearOpMode {
         @Override
         public Mat processFrame(Mat input) {
 
+//Converts the camera color space to HSV for better detection
             Imgproc.cvtColor(input, HSV, Imgproc.COLOR_RGB2HSV);
 
+//Copies the input to the output
             input.copyTo(outPut);
 
+//Creates the rectangles
             Rect rectLeft = new Rect(rectLeftx, rectLefty, rectLeftWidth, rectLeftHeight);
             Rect rectRight = new Rect(rectRightx, rectRighty, rectRightWidth, rectRightHeight);
             Rect rectCenter = new Rect(rectCenterx,rectCentery, rectCenterWidth, rectCenterHeight);
 
+//Gives the rectangles a blue boarder
             Scalar rectangleColor = new Scalar(0,0, 255);
 
+//Draws out the rectangles to scan for yellow
             Imgproc.rectangle(outPut, rectLeft, rectangleColor, 2);
             Imgproc.rectangle(outPut, rectRight, rectangleColor, 2);
             Imgproc.rectangle(outPut, rectCenter, rectangleColor, 2);
 
+//Turns the images of what the rectangles see into a submat that will be used to find the average
             cropLeft = HSV.submat(rectLeft);
             cropRight = HSV.submat(rectRight);
             cropCenter = HSV.submat(rectCenter);
 
+//Extracts the color from the submats, which will be used to find the average
             Core.extractChannel(cropLeft, cropLeft, 2);
             Core.extractChannel(cropRight, cropRight, 2);
             Core.extractChannel(cropCenter, cropCenter, 2);
 
+//Averages each of the images that the rectangles see into a singular value
             Scalar leftAverage = Core.mean(cropLeft);
             Scalar rightAverage = Core.mean(cropRight);
             Scalar centerAverage = Core.mean(cropCenter);
 
+//Turns the values given from the average into a variable
             double finalLeftAverage = leftAverage.val[0];
             double finalRightAverage = rightAverage.val[0];
             double finalCenterAverage = centerAverage.val[0];
 
-
+//If a certain rectangle has a higher value than the other two rectangles, the duck is in that certain rectangle
             if(finalCenterAverage > finalRightAverage && finalCenterAverage > finalLeftAverage) {
                 position = 0;
             } else if (finalLeftAverage > finalCenterAverage && finalLeftAverage > finalRightAverage) {
@@ -252,14 +281,10 @@ public class autoRed extends LinearOpMode {
             } else if (finalRightAverage > finalCenterAverage && finalRightAverage > finalCenterAverage) {
                 position = 1;
             }
-
             return outPut;
             }
-            public Mat getTheResults() { return outPut; }
-
     }
-
-
+//A part of the auto selector that determines which side the robot is on for the red alliance
     private void startingPosition() {
         telemetry.addLine("Welcome to Tim's auto selector!");
         telemetry.addLine("Choose side, left on D-pad for left, right on D-pad for right");
@@ -282,6 +307,7 @@ public class autoRed extends LinearOpMode {
             start = PoseStorage.leftAutoRed;
         }
     }
+//A part of the auto selector that determines where to park
     private void parkingPosition() {
         if (startingPosition == -1) {
             telemetry.addLine("Left side selected, where would you like to park?");
@@ -304,6 +330,7 @@ public class autoRed extends LinearOpMode {
             if (isStopRequested()) return;
         }
     }
+//A part of the auto selector that determines where to park inside the warehouse if the warehouse is selected
     private void wareHousePosition() {
         telemetry.addLine("Warehouse selected, where would you like to park specifically?");
         telemetry.addLine("Press right on D-pad to park on the right side near the wall");
