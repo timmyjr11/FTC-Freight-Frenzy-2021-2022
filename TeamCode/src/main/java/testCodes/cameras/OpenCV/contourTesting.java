8f778303b06d630f7da2a955be0c4e00a2ae67a5
timmyjr11 <timmyytjr@gmail.com>
2022-03-25 14:20:29 -0700

    Contour code version one works

---
+++ TeamCode/src/main/java/testCodes/cameras/OpenCV/contourTesting.java
@@ -0,0 +1,166 @@
+package testCodes.cameras.OpenCV;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.RotatedRect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera2;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+@TeleOp
+@Config
+public class contourTesting extends LinearOpMode {
+    private final FtcDashboard dashboard = FtcDashboard.getInstance();
+    OpenCvInternalCamera2 phoneCam;
+    contourPipe pipeline;
+
+    @Override
+    public void runOpMode() {
+        /**
+         * NOTE: Many comments have been omitted from this sample for the
+         * sake of conciseness. If you're just starting out with EasyOpenCv,
+         * you should take a look at {@link InternalCamera2Example} or its
+         * webcam counterpart, {@link WebcamExample} first.
+         */
+
+        // Create camera instance
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera2(OpenCvInternalCamera2.CameraDirection.BACK, cameraMonitorViewId);
+
+        // Open async and start streaming inside opened callback
+        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+
+                pipeline = new contourPipe();
+                phoneCam.setPipeline(pipeline);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+        FtcDashboard.getInstance().startCameraStream(phoneCam, 30);
+
+
+        // Tell telemetry to update faster than the default 250ms period :)
+        telemetry.setMsTransmissionInterval(20);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            sleep(20);
+
+            telemetry.update();
+        }
+    }
+
+    static class contourPipe extends OpenCvPipeline {
+        static final int CB_CHAN_MASK_THRESHOLD = 80;
+        static final double DENSITY_UPRIGHT_THRESHOLD = 0.03;
+
+        static final Scalar TEAL = new Scalar(3, 148, 252);
+        static final Scalar PURPLE = new Scalar(158, 52, 235);
+        static final Scalar RED = new Scalar(255, 0, 0);
+        static final Scalar GREEN = new Scalar(0, 255, 0);
+        static final Scalar BLUE = new Scalar(0, 0, 255);
+
+        static final int CONTOUR_LINE_THICKNESS = 2;
+        static final int CB_IDX = 2;
+
+        Mat cbMat = new Mat();
+        Mat thresholdMat = new Mat();
+        Mat morphedThreshold = new Mat();
+        Mat contoursOnPlainImageMat = new Mat();
+
+        Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
+        Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(6, 6));
+
+        @Override
+        public Mat processFrame(Mat input) {
+
+            for(MatOfPoint contour : findContours(input)) {
+                analyzeContour(contour, input);
+            }
+            return input;
+        }
+
+        ArrayList<MatOfPoint> findContours(Mat input) {
+            // A list we'll be using to store the contours we find
+            ArrayList<MatOfPoint> contoursList = new ArrayList<>();
+
+            // Convert the input image to YCrCb color space, then extract the Cb channel
+            Imgproc.cvtColor(input, cbMat, Imgproc.COLOR_RGB2YCrCb);
+            Core.extractChannel(cbMat, cbMat, CB_IDX);
+
+            // Threshold the Cb channel to form a mask, then run some noise reduction
+            Imgproc.threshold(cbMat, thresholdMat, CB_CHAN_MASK_THRESHOLD, 255, Imgproc.THRESH_BINARY_INV);
+            morphMask(thresholdMat, morphedThreshold);
+
+            // Ok, now actually look for the contours! We only look for external contours.
+            Imgproc.findContours(morphedThreshold, contoursList, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+
+            // We do draw the contours we find, but not to the main input buffer.
+            input.copyTo(contoursOnPlainImageMat);
+            Imgproc.drawContours(contoursOnPlainImageMat, contoursList, -1, BLUE, CONTOUR_LINE_THICKNESS, 8);
+
+            return contoursList;
+        }
+
+        void morphMask(Mat input, Mat output) {
+            /*
+             * Apply some erosion and dilation for noise reduction
+             */
+
+            Imgproc.erode(input, output, erodeElement);
+            Imgproc.erode(output, output, erodeElement);
+
+            Imgproc.dilate(output, output, dilateElement);
+            Imgproc.dilate(output, output, dilateElement);
+        }
+
+        void analyzeContour(MatOfPoint contour, Mat input){
+            // Transform the contour to a different format
+            Point[] points = contour.toArray();
+            MatOfPoint2f contour2f = new MatOfPoint2f(contour.toArray());
+
+            // Do a rect fit to the contour, and draw it on the screen
+            RotatedRect rotatedRectFitToContour = Imgproc.minAreaRect(contour2f);
+            drawRotatedRect(rotatedRectFitToContour, input);
+        }
+
+        static void drawRotatedRect(RotatedRect rect, Mat drawOn)
+        {
+            /*
+             * Draws a rotated rect by drawing each of the 4 lines individually
+             */
+
+            Point[] points = new Point[4];
+            rect.points(points);
+
+            for(int i = 0; i < 4; ++i)
+            {
+                Imgproc.line(drawOn, points[i], points[(i+1)%4], RED, 2);
+            }
+        }
+    }
+}
